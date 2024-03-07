b5a161819a8c4ff6f16695c90e6e74e1e0b4e6ea
Vlada <proskurina.vlada@gmail.com>
2024-03-07 19:31:46 +0200

    Create app

---
+++ src/main/java/ua/teamchallenge/stor/StorApplication.java
@@ -0,0 +1,13 @@
+package ua.teamchallenge.stor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StorApplication.class, args);
+    }
+
+}
