dda5513c61f094849c21ca79088714457e2c40cf
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-03-23 13:21:29 +0200

    aving changes before merging with master

--- src/main/java/ua/teamchallenge/stor/service/product/impl/ProductServiceImpl.java
+++ src/main/java/ua/teamchallenge/stor/service/product/impl/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package ua.teamchallenge.stor.service.product.impl;
 
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ua.teamchallenge.stor.persistence.entity.Product;
