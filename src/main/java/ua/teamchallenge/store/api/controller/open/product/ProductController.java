20ae46e6195bfe7e5ea127b424780a16ac9e8123
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-03-23 13:39:25 +0200

    Fix product getting from UserController into ProductController

--- src/main/java/ua/teamchallenge/store/api/controller/open/product/ProductController.java
+++ src/main/java/ua/teamchallenge/store/api/controller/open/product/ProductController.java
@@ -1,15 +1,38 @@
 package ua.teamchallenge.store.api.controller.open.product;
 
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ua.teamchallenge.store.api.dto.response.product.ProductDto;
+import ua.teamchallenge.store.facade.product.ProductPdpFacade;
+import ua.teamchallenge.store.facade.product.ProductPlpFacade;
+
+import java.util.List;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping(path = "/api/open/products")
 public class ProductController {
 
-    @GetMapping
-    public String test() {
-        return "Product get";
+    private ProductPdpFacade productPdpFacade;
+    private ProductPlpFacade productPlpFacade;
+
+    // get all products
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        List<ProductDto> products = productPlpFacade.findAllProducts();
+        return ResponseEntity.status(HttpStatus.OK).body(products);
     }
+
+    // get one product
+    @GetMapping("/product/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id) {
+        ProductDto productDto = productPdpFacade.findProductById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(productDto);
+    }
+
 }
