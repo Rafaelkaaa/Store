5c622f98a2d518c59cd79a83bc486369e88b8149
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-03-23 13:22:41 +0200

    ix double store folder

--- src/main/java/ua/teamchallenge/stor/facade/product/ProductFacade.java
+++
@@ -1,7 +0,0 @@
-package ua.teamchallenge.stor.facade.product;
-
-import ua.teamchallenge.stor.api.dto.response.product.ProductDto;
-
-public interface ProductFacade {
-    void create(ProductDto productDto);
-}
