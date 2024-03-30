911ceb0457a5b7f6bc2fbf80b55fe55a8cbdb683
Denys Harkavenko <garkavenkodenispost@gmail.com>
2024-03-30 12:50:10 +0100

    complete basic implementation

---
+++ frontend/src/pages/ErrorPage.tsx
@@ -0,0 +1,11 @@
+import { Box, Heading } from '@chakra-ui/react'
+
+const ErrorPage: React.FC = () => {
+	return (
+		<Box>
+			<Heading color='red'>404 Error</Heading>
+		</Box>
+	)
+}
+
+export default ErrorPage
