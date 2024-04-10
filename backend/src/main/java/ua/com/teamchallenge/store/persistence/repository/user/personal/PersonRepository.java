618f4a024e7e9c853455047ef007b02a9977a5b2
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-04-10 23:44:29 +0300

    10.04.2024 fix something

--- backend/src/main/java/ua/com/teamchallenge/store/persistence/repository/user/personal/PersonRepository.java
+++ backend/src/main/java/ua/com/teamchallenge/store/persistence/repository/user/personal/PersonRepository.java
@@ -1,6 +1,6 @@
 package ua.com.teamchallenge.store.persistence.repository.user.personal;
 
-import ua.com.teamchallenge.store.persistence.entity.user.personal.Personal;
+import ua.com.teamchallenge.store.persistence.entity.user.personal.Person;
 import ua.com.teamchallenge.store.persistence.repository.user.UserRepository;
 
-public interface PersonalRepository extends UserRepository<Personal> { }
+public interface PersonalRepository extends UserRepository<Person> { }
--- backend/src/main/resources/db/migration/V1__create_table_product.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS products
-(
-    id          SERIAL PRIMARY KEY,
-    title       VARCHAR(100)     NOT NULL,
-    price       DOUBLE PRECISION NOT NULL,
-    description VARCHAR(255)     NOT NULL
-);
