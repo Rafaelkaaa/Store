9708efcc9fb47ba2212f514a79cf338fccba8d33
artem <artemmakarenko12@gmail.com>
2024-04-10 16:33:18 +0300

    commit 10.04.24

--- backend/src/main/java/ua/com/teamchallenge/store/config/security/AuthenticationService.java
+++ backend/src/main/java/ua/com/teamchallenge/store/config/security/AuthenticationService.java
@@ -1,7 +1,6 @@
 package ua.com.teamchallenge.store.config.security;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
