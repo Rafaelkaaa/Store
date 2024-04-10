48835dd427dc9bbac8450cdcc62024ad844e1e07
artem <artemmakarenko12@gmail.com>
2024-04-10 16:33:18 +0300

    commit 10.04.24

--- backend/src/main/java/ua/com/teamchallenge/store/facade/product/ProductPlpFacade.java
+++ backend/src/main/java/ua/com/teamchallenge/store/facade/product/ProductPlpFacade.java
@@ -3,8 +3,6 @@ package ua.com.teamchallenge.store.facade.product;
 import reactor.core.publisher.Flux;
 import ua.com.teamchallenge.store.api.dto.response.product.ProductDto;
 
-import java.util.List;
-
 public interface ProductPlpFacade {
 
     Flux<ProductDto> findAll();
