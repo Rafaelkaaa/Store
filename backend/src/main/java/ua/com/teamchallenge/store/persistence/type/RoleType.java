f8e09cb6ffd48a5b87609ee53cc25cd1291e71c8
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-05-04 03:25:02 +0300

    refactor register method and remove from jwt no useles information

--- backend/src/main/java/ua/com/teamchallenge/store/persistence/type/RoleType.java
+++ backend/src/main/java/ua/com/teamchallenge/store/persistence/type/RoleType.java
@@ -5,33 +5,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Set;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 @RequiredArgsConstructor
 public enum RoleType {
+    PERSONAL,
+    ADMIN;
 
-    PERSONAL(Set.of(
-            PermissionType.PERSON_CREATE,
-            PermissionType.PERSON_READ,
-            PermissionType.PERSON_UPDATE,
-            PermissionType.PERSON_DELETE)),
-    ADMIN(Set.of(
-            PermissionType.ADMIN_CREATE,
-            PermissionType.ADMIN_READ,
-            PermissionType.ADMIN_UPDATE,
-            PermissionType.ADMIN_DELETE));
-
-    private final Set<PermissionType> permissions;
-
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        var authorities = getPermissions()
-                .stream()
-                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
-                .collect(Collectors.toList());
-        authorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
-        return authorities;
+    public Collection<GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.name()));
     }
+
 }
